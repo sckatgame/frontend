5d53d1a9d2098bb6f98fbe18d703502db36f9563
Danilo <danilo181201@gmail.com>
2021-05-04 20:18:29 -0300

    Page Logon

---
+++ src/pages/Logon/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+import Header from '../../componets/Header';
+import Form from '../../componets/Form';
+
+export default function Logon(){
+    return(
+        <div className="Logon">
+            <Header/>
+            <Form/>
+        </div>
+    )
+}
