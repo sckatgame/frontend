05c54171031685d80e027d3f19f134ab7d77e9fd
Danilo <danilo181201@gmail.com>
2021-05-06 13:42:01 -0300

    Routa de Validações

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Logon from './pages/Logon';
+import Forgot from './pages/Forgot';
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Logon}/>
+                <Route path="/:validate" component={Forgot}/>
             </Switch>
         </BrowserRouter>
     );
