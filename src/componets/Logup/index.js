386de434ce2969d86e8ffee181fd706e78ca8a88
Danilo <danilo181201@gmail.com>
2021-05-06 14:43:16 -0300

    Logup e Validate

---
+++ src/componets/Logup/index.js
@@ -0,0 +1,86 @@
+import {useState} from 'react'
+import login from '../../assets/login.png';
+import api from '../../services/api'
+import {useHistory} from 'react-router-dom'
+
+export default function Logup(){
+
+    const history = useHistory();
+
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [senha,setSenha] = useState('');
+    
+    const [text,setText] = useState('Cadastrar');
+    const [desable,setDesable] = useState(false);
+
+    async function handleRegister(e){
+        e.preventDefault();
+
+        setText('Aguarde...')
+        setDesable(true);
+
+        try{
+            await api.post('user',{
+                name,
+                email,
+                password:senha
+            });
+
+            history.push(`/validate?email=${email}`)
+
+        }catch(err){
+            const {data} = err.response
+            
+            if(!data.error){
+                alert('Algo deu errado, tente novamente')
+            }else{
+                alert(data.error)
+            }
+
+            setText('Cadastrar')
+            setDesable(false)
+        }
+
+    }
+
+    return(
+        <>
+            <div className="image-icon">
+                <img alt="" src={login}/>
+            </div>
+            
+            <form className="form" onSubmit={handleRegister}>
+                
+                <input 
+                    required
+                    placeholder="Nome de Usuário"
+                    value={name}
+                    onChange={e => setName(e.target.value)} 
+                />
+                
+                <input 
+                    required 
+                    placeholder="Email" 
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}     
+                />
+                    
+                <input 
+                    required 
+                    placeholder="Senha" 
+                    type="password"
+                    minLength="8"
+                    value={senha}
+                    onChange={e => setSenha(e.target.value)}  
+                />
+                
+                <div className="buttons-actions">
+                    <button disabled={desable} className="buttons" style={{width:90}}>{text}</button>
+                </div>
+                
+            </form>
+        </>
+    );
+}
---
+++ src/componets/Validate/index.js
@@ -0,0 +1,67 @@
+import {useState} from 'react'
+import {useHistory} from 'react-router-dom'
+import validate from '../../assets/validate.png';
+import api from '../../services/api'
+
+export default function Validate({email}){
+
+    const history = useHistory();
+
+    const [validate_code,setValidate] = useState('');
+    const [desable,setDesable] = useState(false);
+    const [text,setText] = useState('Confirmar email');
+
+
+    async function handleValidate(e){
+        e.preventDefault();
+        
+        setDesable(true);
+        setText('Aguarde...')
+
+        try{
+            const {data} = await api.post('validate',{code:validate_code,email});
+            localStorage.setItem('token', data.authorization);
+            history.push('/profil');
+            
+
+        }catch(err){
+            const {data} = err.response
+            
+            if(!data.error){
+                alert('Email ou código inválido')
+            }else{
+                alert(data.error)
+            }
+
+            setText('Confirmar email');
+            setDesable(false);
+        }
+    }
+
+    return (
+        <section className="content">
+            <div className="image-icon">
+                <img alt="" style={{width:'55%',height:'60%'}} src={validate}/>
+            </div>
+            
+            <form className="form" onSubmit={handleValidate}>
+                
+                <label>{email}</label>
+                
+                <input
+                    minLength="4"
+                    maxLength="4" 
+                    required
+                    placeholder="código"
+                    type="text"
+                    value={validate_code}
+                    onChange={e => setValidate(e.target.value)} 
+                />
+                
+                <div className="buttons-actions">
+                    <button disabled={desable} className="buttons" style={{width:150}} type="submit">{text}</button>
+                </div>
+            </form>        
+        </section>
+    );
+}
