f516da1517e889e155a65ef4da95d60bb82cee88
Danilo <danilo181201@gmail.com>
2021-05-06 14:24:32 -0300

    PrivateRoute update

--- src/componets/PrivateRouter/index.js
+++ src/componets/PrivateRouter/index.js
@@ -1,13 +1,13 @@
 import {Route, Redirect} from 'react-router-dom'
 import isLogin from '../../services/isLogin'
 
-export default function PrivateRouter({component: Component, ...rest}){
+export default function PrivateRouter({topath,component: Component, ...rest}){
     return <Route {...rest}
         render={
             props => 
                 isLogin() ? 
                 <Component {...props} /> :
-                <Redirect to="/"/>
+                <Redirect to={topath}/>
         }
     />
 }
