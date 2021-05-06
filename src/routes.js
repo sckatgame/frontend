3d112cebd5b67ef242d7588c9e624beae3b84af5
Danilo <danilo181201@gmail.com>
2021-05-06 14:18:46 -0300

    Route Profil

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import PrivateRouter from "./componets/PrivateRouter";
 
 import Logon from './pages/Logon';
 import Forgot from './pages/Forgot';
+import Profil from "./pages/Profil";
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Logon}/>
+                <PrivateRouter path="/profil" component={Profil}/>
                 <Route path="/:validate" component={Forgot}/>
             </Switch>
         </BrowserRouter>
