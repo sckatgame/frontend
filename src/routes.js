562ea4dd8d30a3c59960420134b86a8072eff981
Danilo <danilo181201@gmail.com>
2021-05-06 14:33:08 -0300

    Rotas Publicas e Privadas

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import PrivateRouter from "./componets/PrivateRouter";
+import PublicRoute from "./componets/PublicRoute";
 
 import Logon from './pages/Logon';
 import Forgot from './pages/Forgot';
@@ -9,8 +10,8 @@ export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Logon}/>
-                <PrivateRouter path="/profil" component={Profil}/>
+                <PublicRoute path="/" exact component={Logon}/>
+                <PrivateRouter topath="/" path="/profil" component={Profil}/>
                 <Route path="/:validate" component={Forgot}/>
             </Switch>
         </BrowserRouter>
