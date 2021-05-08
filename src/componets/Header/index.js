7488c0c634f3e8f32acdff94d32903307b041029
Danilo <danilo181201@gmail.com>
2021-05-07 21:12:07 -0300

    Botão para pagina regras

--- src/componets/Header/index.js
+++ src/componets/Header/index.js
@@ -1,11 +1,20 @@
 import './index.css'
 
+import {useHistory} from 'react-router-dom'
+
 export default function Header(){
+    
+    const history = useHistory();
+
     return(
         <div className="Header">
             <h1>Sckat Game</h1>
             <div>
-                <button className="buttons" style={{width:90}}>Regras</button>
+                <button className="buttons" style={{width:90}} onClick={
+                    () =>{
+                        history.push('/rules')
+                    }
+                } >Regras</button>
                 <button className="buttons" style={{width:110}}>Jogar agora</button>
             </div>
         </div>
