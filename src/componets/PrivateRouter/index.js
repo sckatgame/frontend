ca46c8c235ef342f5b8ce3d44c396ed7eb2bb026
Danilo <danilo181201@gmail.com>
2021-05-11 21:18:59 -0300

    Passar o client para os componentes

--- src/componets/PrivateRouter/index.js
+++ src/componets/PrivateRouter/index.js
@@ -1,12 +1,12 @@
 import {Route, Redirect} from 'react-router-dom'
 import isLogin from '../../services/isLogin'
 
-export default function PrivateRouter({topath,component: Component, ...rest}){
+export default function PrivateRouter({client,topath,component: Component, ...rest}){
     return <Route {...rest}
         render={
             props => 
                 isLogin() ? 
-                <Component {...props} /> :
+                <Component {...props} client={client}/> :
                 <Redirect to={topath}/>
         }
     />
