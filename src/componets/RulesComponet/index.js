0b672111a22601a7e8e088354d21e427e3e2470f
Danilo <danilo181201@gmail.com>
2021-06-03 19:15:48 -0300

    Renderizar Ganhador e perdedor

--- src/componets/RulesComponet/index.js
+++ src/componets/RulesComponet/index.js
@@ -1,6 +1,44 @@
+import winner from '../../assets/win.png'
+import loser from '../../assets/lose.webp'
+
 import './index.css'
 
-export default function RulesComponet({round = 1,player1,player2,rule,con = 5,you = 0,opt = 0}){
+export default function RulesComponet({round = 1,player1,player2,rule,con = 5,win,you}){
+    
+    function renderWin(){
+        if(!win){
+            return(
+                <div className="conteiner-placa">
+                    <div className="conteiner-info">
+                        <h3>{player1}</h3>
+                    </div>
+                    <h2>X</h2>
+                    <div className="conteiner-info">
+                        <h3>{player2}</h3>
+                    </div>
+                </div>
+            )
+        }else{
+            if(win === you){
+                return(
+                    <div className="conteiner-placa">
+                        <img alt="" src={winner}/>
+                        <br/>
+                        <h3>Parabéns!!!<br/>Vc ganhou o round anterior</h3>
+                    </div>
+                )
+            }else{
+                return (
+                    <div className="conteiner-placa">
+                        <img alt="" src={loser}/>
+                        <br/>
+                        <h3>Que pena!!!<br/>Vc perdeu o round anterior</h3>
+                    </div>
+                )
+            }
+        }
+    }
+    
     return(
         <div className="conteiner">
             
@@ -10,16 +48,7 @@ export default function RulesComponet({round = 1,player1,player2,rule,con = 5,yo
                 <div className="conteiner-rules">
                     <h3># Vence o jodador que: <br/>{rule}</h3>
                 </div>
-
-                <div className="conteiner-placa">
-                    <div className="conteiner-info">
-                        <h3>{player1}</h3> <h3> Vitorias: {you}</h3>
-                    </div>
-                    <h2>X</h2>
-                    <div className="conteiner-info">
-                        <h3>{player2}</h3> <h3> Vitorias: {opt}</h3>
-                    </div>
-                </div>
+                {renderWin()}
                 <div className="count">
                     <h3># Início em: {con}s</h3>
                 </div>
